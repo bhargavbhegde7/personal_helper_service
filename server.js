299e6c9f3e00e10c3e5f098922e7548745194853
bhargav <bhargavbhegde7@gmail.com>
2019-03-23 06:07:57 +0000

    ngrok link holder, server port update

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
+	console.log("request incoming . . .");
 	res.sendFile(path.join(__dirname, '.', './index.html'));
 });
 
 app.post('/',function(req, res){
+	console.log("request incoming");
 	res.send('have some patience. haven\'t written the post handler yet');
 });
 
-var server = app.listen(3500, function(){
+var server = app.listen(8080, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 
